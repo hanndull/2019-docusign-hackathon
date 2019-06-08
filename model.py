f11f7c670a4610f14a5146dc8d3e4a7b3f4c30a5
vagrant <vagrant@vagrant.vm>
2019-06-08 21:22:12 +0000

    2:22pm acl

--- model.py
+++ model.py
@@ -3,4 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-db = SQLAlchemy(a
+db = SQLAlchemy(app)
+
+
+
+
+if __name__ == "__main__":
+    app.debug = False
+
+
+
